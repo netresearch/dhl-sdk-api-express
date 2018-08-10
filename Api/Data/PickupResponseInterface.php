fb562fbb2c882699de792f046e91adfb710a4967
Ronny Gertler <ronny.gertler@netresearch.de>
2018-08-10 10:21:07 +0200

    DHLGW-53: Pickup API Response classes

---
+++ Api/Data/PickupResponseInterface.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+namespace Dhl\Express\Api\Data;
+
+/**
+ * Pickup Response Interface.
+ *
+ * DTO that carries relevant data for processing the pickup result.
+ *
+ * @api
+ * @package  Dhl\Express\Api
+ * @author   Ronny Gertler <ronny.gertler@netresearch.de>
+ * @license  https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link     https://www.netresearch.de/
+ */
+interface PickupResponseInterface
+{
+    /**
+     * Return the code.
+     *
+     * @return string
+     */
+    public function getCode(): string;
+
+    /**
+     * Return the message.
+     *
+     * @return string
+     */
+    public function getMessage(): string;
+}
---
+++ Model/PickupResponse.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+
+namespace Dhl\Express\Model;
+
+use Dhl\Express\Api\Data\PickupResponseInterface;
+
+/**
+ * Pickup response.
+ *
+ * @package  Dhl\Express\Model
+ * @author   Ronny Gertler <ronny.gertler@netresearch.de>
+ * @license  https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link     https://www.netresearch.de/
+ */
+class PickupResponse implements PickupResponseInterface
+{
+    /**
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * PickupResponse constructor.
+     *
+     * @param string $code
+     * @param string $message
+     */
+    public function __construct(string $code, string $message)
+    {
+        $this->code = $code;
+        $this->message = $message;
+    }
+
+    /**
+     * Return the code.
+     *
+     * @return string
+     */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    /**
+     * Return the message.
+     *
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
