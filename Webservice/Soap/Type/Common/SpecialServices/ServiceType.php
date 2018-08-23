074a022730c35f5cbb1ef028432d47b17064babd
Paul Siedler <paul.siedler@netresearch.de>
2018-08-23 10:42:27 +0200

    DHLGW-99: implement workaround for insurance service

--- Webservice/Soap/Type/Common/SpecialServices/ServiceType.php
+++ Webservice/Soap/Type/Common/SpecialServices/ServiceType.php
@@ -17,12 +17,13 @@ use Dhl\Express\Webservice\Soap\ValueInterface;
  */
 class ServiceType implements ValueInterface
 {
-    private const NUMBER_OF_CHARS = 2;
+    private const NUMBER_OF_CHARS = 3;
 
     /**
      * Available service types.
      */
-    public const TYPE_INSURANCE = 'II';
+    /** @var string @TODO(nr) the code is actually II, but the API does not recognize it currently, therefore use INS */
+    public const TYPE_INSURANCE = 'INS';
 
     /**
      * The service type.
